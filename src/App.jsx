b9d29d737fc57664aec7ced56cdf81adef3818aa
kr-younghoon <sullanta0802@gmail.com>
2024-03-08 03:12:34 +0900

    [#3] Add: react-camera Library 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import React from 'react';
+import Webcam from 'react-webcam';
 import './App.css';
 
 export default function App() {
-    return;
+    const videoConstraints = {
+        width: 1280,
+        height: 720,
+        facingMode: 'user',
+    };
+
+    const webcamRef = React.useRef(null);
+    const capture = React.useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+    }, [webcamRef]);
+
+    return (
+        <>
+            <Webcam
+                audio={false}
+                height={720}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                width={1280}
+                videoConstraints={videoConstraints}
+            />
+            <button onClick={capture}>Capture photo</button>
+        </>
+    );
 }
