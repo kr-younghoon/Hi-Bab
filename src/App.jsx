80357a75eaa4160cc0af2479a756036bcf9f4b8c
kr-younghoon <sullanta0802@gmail.com>
2024-03-09 03:23:10 +0900

    [#3] Feat: Switch Camera 기능 추가 및 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,35 @@ import React from 'react';
 import Webcam from 'react-webcam';
 import './App.css';
 
+const FACING_MODE_USER = 'user';
+const FACING_MODE_ENVIRONMENT = 'environment';
+
+const videoConstraints = {
+    facingMode: FACING_MODE_USER,
+};
+
 export default function App() {
-    const videoConstraints = {
-        width: 1280,
-        height: 720,
-        facingMode: 'user',
-    };
+    const [facingMode, setFacingMode] = React.useState(FACING_MODE_USER);
 
-    const webcamRef = React.useRef(null);
-    const capture = React.useCallback(() => {
-        const imageSrc = webcamRef.current.getScreenshot();
-    }, [webcamRef]);
-
+    const handleClick = React.useCallback(() => {
+        setFacingMode((prevState) =>
+            prevState === FACING_MODE_USER
+                ? FACING_MODE_ENVIRONMENT
+                : FACING_MODE_USER
+        );
+    }, []);
     return (
         <>
+            <button onClick={handleClick}>Switch camera</button>
+
             <Webcam
                 audio={false}
-                height={720}
-                ref={webcamRef}
                 screenshotFormat="image/jpeg"
-                width={1280}
-                videoConstraints={videoConstraints}
+                videoConstraints={{
+                    ...videoConstraints,
+                    facingMode,
+                }}
             />
-            <button onClick={capture}>Capture photo</button>
         </>
     );
 }
