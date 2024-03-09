37c86771353e45466a5176584f397b35a80e5511
kr-younghoon <sullanta0802@gmail.com>
2024-03-10 04:53:09 +0900

    Edit: 기능수정/카메라제거 및 랜덤숫자 생성 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import FuncWebcam from './Components/Webcam/FuncWebcam';
 
-export default function App() {
+function App() {
+    const [number, setNumber] = useState(null);
+
+    const generateRandomNumber = () => {
+        const randomNumber = Math.floor(Math.random() * 4) + 1;
+        setNumber(randomNumber); // Update state with random number
+    };
+
+    useEffect(() => {
+        generateRandomNumber();
+    }, []); // Empty dependency array to run only once
+
     return (
-        <>
-            <FuncWebcam />
-        </>
+        <div className="container w-full mx-auto">
+            <div className="App">
+                <button
+                    className="button text-center font-bold p-4 bg-blue-500 text-white"
+                    onClick={() => generateRandomNumber()}
+                >
+                    랜덤 숫자 추첨
+                </button>
+                {number && <p>랜덤 숫자: {number}</p>}
+            </div>
+        </div>
     );
 }
+
+export default App;
