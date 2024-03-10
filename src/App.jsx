a3a8b005e31cff72550c0a7cc4cf88dffe3ccaed
kr-younghoon <sullanta0802@gmail.com>
2024-03-10 13:51:55 +0900

    Feat: 기능규칙 변경2

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         if (isGenerating) {
             intervalId = setInterval(() => {
                 generateRandomNumber();
-            }, 10); // Change the interval time as needed
+            }, 35); // Change the interval time as needed
         }
 
         return () => clearInterval(intervalId);
@@ -27,15 +27,13 @@ function App() {
 
     return (
         <div className="container w-full mx-auto">
-            <div className="App">
-                <button
-                    className="button text-center font-bold p-4 bg-blue-500 text-white"
-                    onClick={toggleGenerating}
-                >
-                    {isGenerating ? '스탑' : '재개'}
-                </button>
-                {number && <p> {number}</p>}
-            </div>
+            <div className="App">{number && <p> {number}</p>}</div>
+            <button
+                className="button text-center font-bold p-4 bg-blue-500 text-white"
+                onClick={toggleGenerating}
+            >
+                {isGenerating ? '스탑' : '재개'}
+            </button>
         </div>
     );
 }
